d8597f89ec8cd170a9cd7184ade2633013592547
Mauro Cedes <mauro.cedes1994@gmail.com>
2023-11-29 00:59:35 -0300

    se crearon componentes Navbar y Cart

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,13 @@
+import { BsCart4 } from "react-icons/bs";
+
+const Cart = () => {
+    return (
+        <div>
+            <button className="">
+                <BsCart4 size={50}/>
+            </button>
+        </div>
+    )
+}
+
+export default Cart
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+
+import logo from "../../assets/logo.png"
+import Cart from "../Cart/Cart"
+
+const Navbar = () => {
+    return (
+        <div>
+            <header className='flex justify-between bg-zinc-950 items-center'>
+                <img className='w-40 h-40 my-5' src={logo} alt="alt" />
+                <h1 className="font-custom text-slate-200 text-5xl">YOUR STYLE SOCKS</h1>
+                <div className="text-slate-200 flex mx-5 gap-4 items-center">
+                    <a href="#">Inicio</a>
+                    <a href="#">Contacto</a>
+                    <Cart/>
+                </div>
+
+
+
+
+            </header>
+
+
+        </div>
+    )
+}
+
+export default Navbar
